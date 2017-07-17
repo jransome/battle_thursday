ed38b0c67b3c182490cffa56f9586b5e61a83cd8
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-17 17:09:58 +0100

    first commit - add sinatra unsing froms

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'shotgun'
+get '/' do
+  "Hello World"
+end
+
+get '/random_cat' do
+  @names = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+
+get '/named_cat' do
+  @names = params[:name]
+  erb(:index)
+end
