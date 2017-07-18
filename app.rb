a9b29955d847171484dbecc7f78b5e0399f81784
nandhini <nandhini31@gmail.com>
2017-07-18 14:51:46 +0100

    Challenge 1

--- app.rb
+++ app.rb
@@ -1,20 +1,5 @@
 require 'sinatra'
-require 'shotgun'
 
 get '/' do
-  "Hello World"
-end
-
-get '/random_cat' do
-  @names = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
-end
-
-get '/form' do
-  erb(:form)
-end
-
-post '/named_cat' do
-  @names = params[:name]
-  erb(:index)
+	erb(:index)
 end
