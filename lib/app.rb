b2c7d54048269c8de119ceca728d846c9b3e496e
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-19 15:47:12 +0100

    add button Player 1 attack Player 2

--- lib/app.rb
+++ lib/app.rb
@@ -16,7 +16,19 @@ class Battle < Sinatra::Base
     player_1_hp, player_2_hp = 100, 100
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
-    erb :play, { locals: { player_1_hp: player_1_hp, player_2_hp: player_2_hp } }
+    last_move = session[:last_move]
+    p last_move
+    attack_confirmation = ''
+    if last_move == 'Attack'
+      attack_confirmation = "#{@player_1_name} has attacked #{@player_2_name}"
+    end
+    p attack_confirmation
+    erb :play, { locals: { player_1_hp: player_1_hp, player_2_hp: player_2_hp, attack_confirmation: attack_confirmation } }
+  end
+
+  post '/attacked' do
+    session[:last_move] = params[:attack]
+    redirect '/play'
   end
 
   run! if app_file == $0
